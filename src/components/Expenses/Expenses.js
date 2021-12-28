244fae437d469db65ff8b945574aca7d09e8c158
mirmahyar <m.mirsamadi@gmail.com>
2021-12-28 23:16:47 +0330

    adding entered expense dynamically to the page

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,22 +1,18 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import "./Expenses.css"
+import "./Expenses.css";
 
 function Expenses(props) {
-  
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.item[0].title}
-        date={props.item[0].date}
-        amount={props.item[0].amount}
-      />
-      <ExpenseItem
-        title={props.item[1].title}
-        date={props.item[1].date}
-        amount={props.item[1].amount}
-      />
+      {props.item.map((expense) => (
+        <ExpenseItem key={expense.id}
+          title={expense.title}
+          date={expense.date}
+          amount={expense.amount}
+        />
+      ))}
     </Card>
   );
 }
